07f4c6839b2a27dc2f20a700280bd6c2ced10dd8
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-28 00:10:27 +0700

    feat: add create user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { UserCreateBodyDto } from './dto/user.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -7,5 +8,19 @@ export class UserService {
 
   public async getUsers() {
     return await this.userRepository.find();
+  }
+
+  public async createUser(createBodyDto: UserCreateBodyDto) {
+    if (
+      await this.userRepository.findOne({ username: createBodyDto.username })
+    ) {
+      throw new UnprocessableEntityException('User already exists');
+    }
+
+    try {
+      return await this.userRepository.createAndSave(createBodyDto);
+    } catch (error) {
+      throw new UnprocessableEntityException(error.message);
+    }
   }
 }
