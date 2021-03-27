f77ba50e42d8b479baf85098461bdefdde5f9d09
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-27 21:47:19 +0700

    feat: add nest-fastify-application and configure nest server to use fastify plus minor changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+/**
+ * * Nest Imports
+ */
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+/**
+ * * App Module Import
+ */
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+class Server {
+  static async bootstrap() {
+    /** Create nest application instance */
+    const nestFastifyApplication = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+    );
+
+    /** Set global prefix for all api route */
+    nestFastifyApplication.setGlobalPrefix('/api/v1');
+
+    /** Run server */
+    await nestFastifyApplication.listen(3000, '0.0.0.0');
+  }
 }
-bootstrap();
+
+/** Bootstrap application */
+Server.bootstrap();
