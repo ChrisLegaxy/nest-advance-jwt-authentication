536d9b12827ee5879fef22675913faad323c480c
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-28 01:05:11 +0700

    feat: add delete user to user service and minor changes

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -4,6 +4,7 @@ import {
   UnprocessableEntityException,
 } from '@nestjs/common';
 import { UserCreateBodyDto, UserUpdateBodyDto } from './dto/user.dto';
+import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 
 @Injectable()
@@ -14,7 +15,7 @@ export class UserService {
     return await this.userRepository.find();
   }
 
-  public async findByIdOrFail(id: string) {
+  public async findByIdOrFail(id: string): Promise<User> {
     try {
       return await this.userRepository.findOneOrFail(id);
     } catch (error) {
@@ -22,7 +23,7 @@ export class UserService {
     }
   }
 
-  public async create(createBodyDto: UserCreateBodyDto) {
+  public async create(createBodyDto: UserCreateBodyDto): Promise<User> {
     const exisitingUsername = await this.userRepository.findOne({
       username: createBodyDto.username,
     });
@@ -38,9 +39,24 @@ export class UserService {
     }
   }
 
-  public async update(id: string, updateBodyDto: UserUpdateBodyDto) {
+  public async update(
+    id: string,
+    updateBodyDto: UserUpdateBodyDto,
+  ): Promise<User> {
+    await this.findByIdOrFail(id);
+
     try {
       return await this.userRepository.createAndUpdate(id, updateBodyDto);
+    } catch (error) {
+      throw new UnprocessableEntityException(error.message);
+    }
+  }
+
+  public async delete(id: string): Promise<void> {
+    await this.findByIdOrFail(id);
+
+    try {
+      await this.userRepository.delete(id);
     } catch (error) {
       throw new UnprocessableEntityException(error.message);
     }
