25096ca1c0fb23ff67a0167e0e4b11ffe7c8867b
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-29 02:47:10 +0700

    feat: add all new auth functionalities in

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
+import { LocalStrategy } from './strategies/login.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { RefreshTokenJwtStrategy } from './strategies/refresh-token-jwt.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: 'MY_SUPER_SECRET_KEY',
+      }),
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy, RefreshTokenJwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
