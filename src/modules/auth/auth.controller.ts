4f40329a60dc662ec9afd836bea1833216ed556a
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-29 04:20:24 +0700

    feat: add proper comments for each sections

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,27 +1,54 @@
+/**
+ * * Packages Imports
+ */
 import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { FastifyReply } from 'fastify';
-import { AuthUser } from 'src/decorators/auth-user.decorator';
-import { RefreshTokenJwtAuthGuard } from 'src/guards/refresh-token-jwt.guard';
+
+/**
+ * * App Module imports
+ */
+import { User } from '../user/user.entity';
+
+/**
+ * * Local Module Imports
+ */
+import { AuthService } from './auth.service';
+
+/**
+ * * Route Guards
+ */
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 import { LocalAuthGuard } from 'src/guards/local-auth.guard';
-import { AuthService } from './auth.service';
-import { AuthResponseDto, RegisterBodyDto } from './dto/auth.dto';
-import { User } from '../user/user.entity';
-import { UserResponseDto } from '../user/dto/user.dto';
+import { RefreshTokenJwtAuthGuard } from 'src/guards/refresh-token-jwt.guard';
+
+/**
+ * * Decorators
+ */
+import { AuthUser } from 'src/decorators/auth-user.decorator';
+
+/**
+ * * Utils
+ */
 import { setRefreshTokenToHttpOnlyCookie } from 'src/utils/response.util';
+
+/**
+ * * Dtos
+ */
+import { UserResponseDto } from '../user/dto/user.dto';
+import { AuthResponseDto, RegisterBodyDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Get('/register')
+  @Post('/register')
   public async register(@Body() registerBodyDto: RegisterBodyDto) {
     return await this.authService.register(registerBodyDto);
   }
 
-  @Post('/login')
   @UseGuards(LocalAuthGuard)
+  @Post('/login')
   public async login(@AuthUser() user: User, @Res() response: FastifyReply) {
     setRefreshTokenToHttpOnlyCookie(
       response,
