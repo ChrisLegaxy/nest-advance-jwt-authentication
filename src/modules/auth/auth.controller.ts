28e759c3732cccf3c005c71271e94da36110f133
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-29 02:46:49 +0700

    feat: add complete auth route functionalities

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { FastifyReply } from 'fastify';
+import { AuthUser } from 'src/decorators/auth-user.decorator';
+import { RefreshTokenJwtAuthGuard } from 'src/guards/refresh-token-jwt.guard';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
+import { LocalAuthGuard } from 'src/guards/local-auth.guard';
+import { AuthService } from './auth.service';
+import { AuthResponseDto, RegisterBodyDto } from './dto/auth.dto';
+import { User } from '../user/user.entity';
+import { UserResponseDto } from '../user/dto/user.dto';
+import { setRefreshTokenToHttpOnlyCookie } from 'src/utils/response.util';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Get('/register')
+  public async register(@Body() registerBodyDto: RegisterBodyDto) {
+    return await this.authService.register(registerBodyDto);
+  }
+
+  @Post('/login')
+  @UseGuards(LocalAuthGuard)
+  public async login(@AuthUser() user: User, @Res() response: FastifyReply) {
+    setRefreshTokenToHttpOnlyCookie(
+      response,
+      await this.authService.signJwtRefreshToken(user),
+    );
+
+    return await response.send(
+      plainToClass(AuthResponseDto, await this.authService.login(user)),
+    );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/current_user')
+  public get(@AuthUser() user: User) {
+    return plainToClass(UserResponseDto, user);
+  }
+
+  @UseGuards(RefreshTokenJwtAuthGuard)
+  @Get('/refresh_token')
+  public async refreshToken(
+    @AuthUser() user: User,
+    @Res() response: FastifyReply,
+  ) {
+    setRefreshTokenToHttpOnlyCookie(
+      response,
+      await this.authService.signJwtRefreshToken(user),
+    );
+
+    return await response.send(
+      plainToClass(AuthResponseDto, await this.authService.login(user)),
+    );
+  }
+}
