07841ec2e8e15095429024fa51b422674caf8795
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-29 04:35:23 +0700

    feat: add base watermark

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,12 @@
 /**
+ * @version 1.0.0
+ * @license MIT
+ * @copyright CPC
+ * @author Chris Legaxy | Chris Van
+ * @contact chris.legaxy@gmail.com | chrisvan.vshmr@gmail.com
+ */
+
+/**
  * * Packages Imports
  */
 import { Injectable } from '@nestjs/common';
