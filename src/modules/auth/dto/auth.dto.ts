e6c970c563e44415ae3340b2b804105150ce1e72
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-29 02:31:19 +0700

    feat: add basic dto for auth module

---
+++ src/modules/auth/dto/auth.dto.ts
@@ -0,0 +1,34 @@
+import { Exclude, Expose } from 'class-transformer';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { UserResponseDto } from 'src/modules/user/dto/user.dto';
+
+@Exclude()
+export class AuthResponseDto {
+  @Expose()
+  user: UserResponseDto;
+
+  @Expose()
+  accessToken: string;
+}
+
+export class RegisterBodyDto {
+  @MinLength(4)
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @MinLength(6)
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
+
+export class LoginBodyDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
