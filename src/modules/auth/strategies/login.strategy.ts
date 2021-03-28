d15a233d06e80a5b5aa0996b9f15c1fcc7438f56
Chris Legaxy <chris_legaxy@yahoo.com>
2021-03-29 04:20:56 +0700

    feat: add proper comments for each sections

--- src/modules/auth/strategies/login.strategy.ts
+++ src/modules/auth/strategies/login.strategy.ts
@@ -1,9 +1,24 @@
+/**
+ * * Packages Imports
+ */
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthService } from '../auth.service';
-import { LoginBodyDto } from '../dto/auth.dto';
+
+/**
+ * * User Module Imports
+ */
 import { User } from 'src/modules/user/user.entity';
+
+/**
+ * * Local Imports
+ */
+import { AuthService } from '../auth.service';
+
+/**
+ * * Dtos
+ */
+import { LoginBodyDto } from '../dto/auth.dto';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
