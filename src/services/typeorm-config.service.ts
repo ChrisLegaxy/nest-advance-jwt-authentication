67fcb1ea06869cbea8a1778bdb9853d57413d5aa
Chris Legaxy <chris_legaxy@yahoo.com>
2021-04-02 00:38:12 +0700

    feat: add typeorm config service

---
+++ src/services/typeorm-config.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return this.configService.get('database');
+  }
+}
