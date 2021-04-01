28b4396e671a799018c43187da85b164376c9d77
Chris Legaxy <chris_legaxy@yahoo.com>
2021-04-02 01:49:04 +0700

    feat: add condition for production

--- src/services/typeorm-config.service.ts
+++ src/services/typeorm-config.service.ts
@@ -7,6 +7,10 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
+    if (this.configService.get('app').env === 'production') {
+      return this.configService.get('database-prod');
+    }
+
     return this.configService.get('database');
   }
 }
