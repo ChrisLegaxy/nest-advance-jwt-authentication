130f8826c8cf0ea3001120173beffac01bc69bf5
Chris Legaxy <chris_legaxy@yahoo.com>
2021-04-02 02:08:00 +0700

    feat: add port in app config

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -2,4 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   env: process.env.NODE_ENV || 'development',
+  port: parseInt(process.env.PORT) || 3000,
 }));
